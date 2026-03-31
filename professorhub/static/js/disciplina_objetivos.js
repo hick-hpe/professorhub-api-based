f53b87cef3bf96746f96089d21e17e15e3b36a15
hick-hpe <hpalermoemerick@gmail.com>
2026-03-31 11:17:16 -0300

    refactor, fix: editar e excluir objetivos

---
+++ professorhub/static/js/disciplina_objetivos.js
@@ -0,0 +1,31 @@
+const modalExcluir = document.getElementById('modalExcluir');
+const modalEditar = document.getElementById('modalEditar');
+
+modalEditar.addEventListener('show.bs.modal', (event) => {
+    const button = event.relatedTarget;
+
+    const descricao = button.getAttribute('data-descricao');
+    const urlEditar = button.getAttribute('data-url');
+
+    const textareaDescricao = modalEditar.querySelector('#objetivo_descricao');
+    textareaDescricao.value = descricao;
+
+    const form = modalEditar.querySelector('form');
+    form.action = urlEditar;
+});
+
+modalExcluir.addEventListener('show.bs.modal', (event) => {
+    const button = event.relatedTarget;
+
+    const descricao = button.getAttribute('data-descricao');
+    const urlExcluir = button.getAttribute('data-url');
+
+    const textareaDescricao = modalExcluir.querySelector('#objetivo_excluir');
+    textareaDescricao.textContent = descricao;
+
+    const form = modalExcluir.querySelector('form');
+    form.action = urlExcluir;
+});
+
+
+
